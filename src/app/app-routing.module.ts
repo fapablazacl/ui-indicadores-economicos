1916d7b32c5bd3794cca0d8d2e9d29b83c72765e
Felipe Apablaza <ing.apablaza@gmail.com>
2020-05-31 17:57:38 -0400

    Agregado componente de resumen de indicadores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndicatorComponent } from './indicator/indicator.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'indicador', component: IndicatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndicatorComponent } from './indicator/indicator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndicatorComponent
   ],
   imports: [
     BrowserModule,
